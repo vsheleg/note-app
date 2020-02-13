8fdd07f3b3a47e6bb9e840d0168c1667c31bf097
Varvara Sheleh <varvara.sheleh@itechart-group.com>
2020-02-13 16:18:47 +0300

    add autoincrement

--- src/server/repositories/index.js
+++ src/server/repositories/index.js
@@ -1,16 +1,25 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("notes", "postgres", "1111", {
+const sequelize = new Sequelize("notes-app", "postgres", "1111", {
   dialect: "postgres"
 });
 
-const Notes = sequelize.define("users", {
+const Notes = sequelize.define("notes", {
   id: {
     type: Sequelize.INTEGER,
+    autoIncrement: true,
     primaryKey: true,
     allowNull: true
   },
   note_content: {
     type: Sequelize.STRING,
+    allowNull: true
+  },
+  createdAt: {
+    type: Sequelize.TIME,
+    allowNull: true
+  },
+  updatedAt: {
+    type: Sequelize.TIME,
     allowNull: true
   }
 });
@@ -27,18 +36,8 @@ function editNote(body, noteId) {
   Notes.update({ note_content: body }, { where: { id: noteId } });
 }
 function addNote(body) {
-  let result = [];
-  Notes.findAll({ raw: true }).then(data => {
-    for (let i = 0; i < data.length; i++) {
-      result.push(data[i].id);
-    }
-    result = result.sort();
-    let newId = Math.max(...result) + 1;
-
-    return Notes.create({
-      id: newId,
-      note_content: body
-    });
+  return Notes.create({
+    note_content: body
   });
 }
 function getNotes() {
