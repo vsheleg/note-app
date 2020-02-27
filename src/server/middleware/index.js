84cbb6ff5e9529acd1517c07e993a9a0bbfb08ad
Varvara Sheleh <varvara.sheleh@itechart-group.com>
2020-02-27 16:04:42 +0300

    chang middleware

--- src/server/middleware/index.js
+++ src/server/middleware/index.js
@@ -2,20 +2,16 @@ const jwt = require("jsonwebtoken"),
   repository = require("../repositories/user.repository");
 
 async function resolveUser(req, res, next) {
-  //checks if there exists token
-  if (req.headers.authorization) {
-    req.userToken = req.headers.authorization;
-    const decoded = jwt.decode(req.userToken);
-    req.decodedUser = decoded || { email: "", password: "" };
-    next();
-  }
+  const { authorization: token } = req.headers;
+  const userId = token ? jwt.decode(token) : null;
+  req.user = userId ? await repository.findUser(userId) : null;
+  next();
 }
 async function validateUser(req, res, next) {
-  //decodes token
-  const user = await repository.findUser(req.decodedUser);
-  if (!user) {
-    res.sendStatus(403); //if such user doesn't exists
+  if (!req.user) {
+    res.sendStatus(403);
+  } else {
+    next();
   }
-  next();
 }
 module.exports = { resolveUser, validateUser };
