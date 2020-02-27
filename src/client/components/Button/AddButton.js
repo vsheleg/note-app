47875d79c8067e4645fd47f31584805b5bb17767
Varvara Sheleh <varvara.sheleh@itechart-group.com>
2020-02-27 16:50:39 +0300

    add func components

--- src/client/components/Button/AddButton.js
+++ src/client/components/Button/AddButton.js
@@ -2,35 +2,31 @@ import React from "react";
 import "./AddButton.css";
 import { render } from "@testing-library/react";
 
-export default class AddButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.textInputRef = React.createRef();
+export default function AddButton(props) {
+  const textInputRef = React.createRef();
+
+  function addNote() {
+    props.onAdd(textInputRef.current.value);
   }
-  addNote = () => {
-    this.props.onAdd(this.textInputRef.current.value);
-  };
-  render() {
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <input
-            className="login"
-            placeholder="Add new item"
-            type="text"
-            id="addNote"
-            name="addNote"
-            ref={this.textInputRef}
-          />
-          <input
-            id="plus"
-            type="button"
-            className="primary"
-            onClick={this.addNote}
-            value="+"
-          />
-        </div>
+        <input
+          className="login"
+          placeholder="Add new item"
+          type="text"
+          id="addNote"
+          name="addNote"
+          ref={textInputRef}
+        />
+        <input
+          id="plus"
+          type="button"
+          className="primary"
+          onClick={addNote}
+          value="+"
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
--- src/client/components/NoteList/Note/Note.js
+++ src/client/components/NoteList/Note/Note.js
@@ -1,70 +1,54 @@
-import React, { createRef } from "react";
+import React, { createRef, useState, useEffect } from "react";
 import noteService from "../../../services/note.service";
 import "./Note.css";
 import "../../../pages/signup/signup.css";
 
-class Note extends React.Component {
-  constructor(props) {
-    super(props);
-    this.textInputRef = React.createRef();
-    this.state = { val: false, content: "", loading: false };
-  }
-  updateItems = () => {
-    let result = noteService.loadNote(this.props.note);
+export default function Note(props) {
+  const [val, setVal] = useState(false);
+  const [content, setContent] = useState("");
+  const textInput = React.createRef();
+
+  const updateItems = () => {
+    let result = noteService.loadNote(props.note);
     result.then(response => {
-      this.setState({
-        val: false,
-        content: response,
-        loading: true
-      });
+      setContent(response);
     });
   };
-  componentDidMount = () => {
-    this.updateItems();
-  };
-  componentDidUpdate = () => {};
-  componentWillUnmount = () => {};
-  deleteItem = () => {
-    this.props.onDelete(this.props.note);
-  };
 
-  editItem = async () => {
-    if (this.state.val) {
-      let newValue = this.textInputRef.current.value;
-      await noteService.editNote(
-        { value: this.textInputRef.current.value },
-        this.props.note
-      );
-      this.setState({
-        content: newValue,
-        val: false
-      });
-    } else {
-      this.setState({ val: true });
-    }
-  };
+  useEffect(() => {
+    updateItems();
+  }, [val]);
 
-  render() {
-    return (
-      <div>
-        <div className="note">
-          <span id="title">{this.props.note}</span>
-          <hr id="title-line" />
-          <div className="note-content">{this.state.content}</div>
-          <input type="button" onClick={this.deleteItem} name="delete" />
-          <input type="button" onClick={this.editItem} name="edit" />
-          {this.state.val ? (
-            <input
-              type="text"
-              name="editNote"
-              id="editNote"
-              ref={this.textInputRef}
-            />
-          ) : null}
-        </div>
-      </div>
-    );
+  function deleteItem() {
+    props.onDelete(props.note);
   }
-}
 
-export default Note;
+  async function editItem() {
+    if (val) {
+      let newValue = textInput.current.value;
+      await noteService.editNote(
+        { value: textInput.current.value },
+        props.note
+      );
+      setContent(newValue);
+      setVal(false);
+    } else {
+      setVal(true);
+    }
+  }
+
+  return (
+    <div>
+      <div className="note">
+        <span id="title">{props.note}</span>
+        <hr id="title-line" />
+        <div className="note-content">{content}</div>
+        <input type="button" onClick={deleteItem} name="delete" />
+        <input type="button" onClick={editItem} name="edit" />
+        {val ? (
+          <input type="text" name="editNote" id="editNote" ref={textInput} />
+        ) : null}
+      </div>
+    </div>
+  );
+}
